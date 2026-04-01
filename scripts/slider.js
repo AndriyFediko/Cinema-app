d273f9332368ca9ad039c7a8178317151972bcec
AndriyFediko <105150222+AndriyFediko@users.noreply.github.com>
2026-04-01 21:47:04 +0300

    Delete scripts directory

--- scripts/slider.js
+++
@@ -1,8 +0,0 @@
-const sliderImage = document.getElementById("hero-slider__image");
-const title = document.getElementById("hero-slider__title");
-
-export function changeImage(film) {
-    sliderImage.src = film.url;
-    sliderImage.alt = `Постер фільму ${film.title}`;
-    title.textContent = film.title;
-}
