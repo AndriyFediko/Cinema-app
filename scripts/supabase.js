44c73bd014c3d341bf84f79b1e6caa8434dbc297
AndriyFediko <105150222+AndriyFediko@users.noreply.github.com>
2026-04-04 16:19:52 +0300

    Delete scripts directory

--- scripts/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
-
-const supabaseUrl = 'https://ylervnsmjrtdfkbcmjky.supabase.co';
-const supabaseKey = 'sb_publishable_qiZNJW-3OkIfWbT7VuSjoQ_sZBziFzP';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
