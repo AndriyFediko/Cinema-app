6b3b73b2e37021a133bc3fa0b0ac308ccce67388
AndriyFediko <105150222+AndriyFediko@users.noreply.github.com>
2026-04-04 16:50:45 +0300

    Delete scripts directory

--- scripts/burgerMenu.js
+++
@@ -1,8 +0,0 @@
-const burgerMenuIcon = document.getElementById("burger-menu__icon");
-const burgerMenu = document.getElementById("burger-menu");
-
-
-burgerMenuIcon.addEventListener("click", () => {
-    burgerMenu.classList.toggle("burger-menu--active");
-    burgerMenuIcon.classList.toggle("burger-menu__icon--active");
-});
